a1fce2e5582f67df74ca2ab42f1d540d5f264d4c
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-10 22:56:50 +0330

    contact us done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Slider from "@/components/Slider";
 import Categories from "../components/Categories";
+import ContactUs from "@/components/ContactUs";
 
 export default function Home() {
   return (
     <div className="flex w-full flex-wrap p-4 justify-center items-start">
       <Categories />
       <Slider />
+      <ContactUs />
     </div>
   );
 }
---
+++ components/ContactUs.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Image from "next/image";
+import shopPic from "../public/images/shop.png";
+import { BsShop } from "react-icons/bs";
+
+const ContactUs = () => {
+  return (
+    <div className="flex flex-wrap w-full max-w-7xl p-8 my-10 text-gray-700">
+      <div className="flex justify-between items-center w-full ">
+        <div className="w-full md:w-1/2">
+          <p className="text-3xl font-bold my-7">{"صاحب کسب و کار هستید؟"}</p>
+          <p className="text-xl my-7">
+            {
+              "با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید."
+            }
+          </p>
+          <div>
+            <button className="flex gap-5 px-4 py-2 bg-pink-600 text-white font-bold text-lg rounded-lg">
+              <BsShop size={"1.5em"} />
+              {"ثبت نام فروشندگان"}
+            </button>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2 md:flex justify-end">
+          <Image src={shopPic} alt="shop pic" width={350} height={350} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactUs;
