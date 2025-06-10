51af9a01f8bad58e5561e970634f0b2216237365
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-10 18:11:52 +0330

    slider done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Slider from "@/components/Slider";
 import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <div className="flex w-full flex-wrap p-4 justify-center items-start">
       <Categories />
+      <Slider />
     </div>
   );
 }
--- components/Categories.tsx
+++ components/Categories.tsx
@@ -7,7 +7,7 @@ import { MdOutlineArrowBackIosNew } from "react-icons/md";
 const Categories = () => {
   return (
     <div className="w-full max-w-7xl flex flex-wrap mt-16 bg-[#f0efe7] py-6 px-3">
-      <div className="w-full ">{"دسته‌بندی‌ها"}</div>
+      <div className="w-full font-bold">{"دسته‌بندی‌ها"}</div>
       <div className="flex flex-wrap w-full py-4">
         {DATABASE.cats.map((cat) => (
           <div
@@ -16,7 +16,7 @@ const Categories = () => {
           >
             <Link href={`/categories/${cat.id}`}>
               <div
-                className="border-4 relative border-white rounded-xl shadow-md hover:shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-[1.01]
+                className="border-4 relative border-white rounded-xl shadow-md hover:shadow-xl transition-transform duration-300 ease-in-out transform hover:scale-[1.0]
 "
               >
                 <Image
@@ -31,9 +31,12 @@ const Categories = () => {
                   }}
                 />
 
-                <div className="flex items-center absolute bottom-0 -right-1 px-2 py-1 text-sm bg-white rounded-tl-xl rounded-br-xl">
+                <div className="group flex items-center absolute bottom-0 -right-1 px-3 py-1 text-sm bg-white rounded-tl-xl box-border rounded-br-xl gap-1">
                   {cat.title}
-                  <MdOutlineArrowBackIosNew style={{ color: "#ff00a6" }} />
+                  <MdOutlineArrowBackIosNew
+                    style={{ color: "#ff00a6" }}
+                    className="transform transition-transform duration-200 ease-in-out group-hover:-translate-x-1"
+                  />
                 </div>
               </div>
             </Link>
