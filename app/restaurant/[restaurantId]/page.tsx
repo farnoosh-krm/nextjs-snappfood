5e2cafec6663106f60d3a3149a468fa977918953
FaRnOoShKaRiMi <farnoosh.karimi99@gmail.com>
2025-06-12 11:35:18 +0330

    restaurant page and menu done

--- app/restaurant/[restaurantId]/page.tsx
+++ app/restaurant/[restaurantId]/page.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import BreadCrump from "@/components/BreadCrump";
+import Menu from "@/components/Menu";
+import { DATABASE } from "@/src/data/data";
+import { convertToPersian } from "@/src/utils/convertToPersian";
+import Image from "next/image";
 
-const restaurantPage = () => {
-  return <div>restaurantPage</div>;
+interface Props {
+  params: Promise<{ restaurantId: string }>;
+}
+
+const restaurantPage = async ({ params }: Props) => {
+  const { restaurantId } = await params;
+  const [restaurants] = DATABASE.restaurants.filter(
+    (rest) => rest.id === +restaurantId
+  );
+
+  return (
+    <div className="flex flex-wrap w-full justify-center p-1 md:p-4 bg-[#f8f8f9]">
+      <div className="flex flex-wrap max-w-7xl w-full my-16">
+        <BreadCrump title={restaurants.name} />
+
+        <div className="flex flex-col md:flex-row p-4 w-full gap-4">
+          <div className="flex items-center gap-4 w-full md:w-1/4">
+            <div className="w-20 h-20 rounded-xl border-1 border-white">
+              <Image
+                src={restaurants.image}
+                alt={restaurants.name}
+                width={100}
+                height={100}
+                style={{ width: "100%", height: "100%", borderRadius: "12px" }}
+              />
+            </div>
+            <div className="font-bold text-sm">{restaurants.name}</div>
+          </div>
+
+          <Menu restaurantId={restaurantId} />
+
+          <div className="flex items-center w-full justify-center md:w-1/4 bg-white shadow-md rounded-xl px-2 py-4 hover:shadow-xl">
+            <Image
+              src={restaurants.delivery.icon}
+              alt="delivery icon"
+              width={16}
+              height={16}
+              className="ml-2"
+            />
+            {`${restaurants.delivery.type} `}
+            {`${convertToPersian(restaurants.delivery.price)}`}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default restaurantPage;
---
+++ components/Menu.tsx
@@ -0,0 +1,59 @@
+import { DATABASE } from "@/src/data/data";
+import React from "react";
+import Image from "next/image";
+import { convertToPersian } from "@/src/utils/convertToPersian";
+
+const Menu = ({ restaurantId }) => {
+  const foods = DATABASE.foods.filter((food) => food.id !== +restaurantId);
+  return (
+    <div className="flex flex-wrap w-full md:w-2/4">
+      <div
+        className="w-full rounded-t-xl py-4 bg-white border-gray-300 border-b-0 text-sm text-center "
+        style={{ borderWidth: "0.5px" }}
+      >
+        {"غذاها"}
+      </div>
+
+      {foods.map((food) => (
+        <div
+          className="flex flex-wrap w-full  md:w-1/2 bg-white p-4 border-gray-300"
+          style={{ borderWidth: "0.5px" }}
+          key={food.id}
+        >
+          {/* //////// */}
+          <div className="flex justify-between w-full">
+            <div className="flex flex-col justify-center ml-2">
+              <div className="font-bold">{food.name}</div>
+              <div className="text-sm mt-2 text-[#474747]">{food.desc}</div>
+            </div>
+
+            <div>
+              <div className="w-28 h-28 rounded-xl">
+                <Image
+                  src={food.image}
+                  alt={food.name}
+                  width={100}
+                  height={100}
+                  style={{ width: "100%", height: "100%" }}
+                  className="rounded-xl"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex w-full mt-4 justify-between items-center">
+            <p className="text-sm text-[#474747]">{`${convertToPersian(
+              food.price
+            )} تومان`}</p>
+
+            <div className="px-8 py-2 bg-white text-pink-500 hover:text-white hover:bg-pink-500 rounded-xl cursor-pointer shadow-xl border-2 border-gray-100 ">
+              {"افزودن"}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Menu;
